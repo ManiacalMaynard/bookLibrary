0f4207d34c43d809733295a653ca5064866249cb
ManiacalMaynard <maynard@maniacalindustries.com>
2022-04-20 17:26:12 -0700

    css tweaks, set up js for button functions

--- app.js
+++ app.js
@@ -10,24 +10,29 @@ function Book (image, title, author, link, inLibrary) {
 };
 
 // Need to add a button that will add a new book object to the end of the bookList array, needs to call BUILDER again to re-populate the cardDeck div container
-function addBook(image, title, author, link, inLibrary) {
-  let book = new Book(image, title, author, link, inLibrary);
-  bookList.push(book)
-}
+function addBook(input) {
+  // let book = new Book(image, title, author, link, inLibrary);
+  // bookList.push(book)
+  console.log(input)
+};
 
 // Need to add a removeBook() button that removes that book instance from the current working array and calls BUILDER to redraw the display
-// const removeBook = (id) => {
-//   bookList.splice(id, 1);
-//   // deckBuilder()
-// }
+const removeBook = (id) => {
+  bookList.splice(id, 1);
+  deckBuilder()
+}
 
 // Need to add a libraryToggle() button that toggles the currentBook.inLibrary state
-// const libraryToggle = (id) => {
-//   let bookInLibrary = bookList.id.inLibrary;
-//   bookInLibrary === true ?
-//   bookInLibrary = false :
-//   bookInLibrary = true ;
-// }
+const libraryToggle = (thing) => {
+
+
+};
+
+const libraryDisplay = (item) => {
+  return item.inLibrary === true ?
+  '🗹' :
+  '□' ;
+};
 
 // The mighty BUILDER function: Iterates through bookList array to build each card and add it to the cardDeck div container
 
@@ -39,18 +44,24 @@ function deckBuilder() {
   const card = document.createElement('div')
     const content = `
     <img src=${item.image} />
-    <h3>${item.title}</h3>
-    <p>by ${item.author}</p>
-    <a href=${item.link} target="_blank"><button>Amazon</button></a>
-    <p>In Library: ${item.inLibrary}</p>
+    <div>
+      <h3>${item.title}</h3>
+      <p>by ${item.author}</p>
+    </div>
+    <div class="btnRow">
+      <button class="deleteBtn" tooltip="DELETE">✖</button>
+      <a href=${item.link} target="_blank"><button>Amazon</button></a>
+      <button class="toggleBtn">${libraryDisplay(item)}</button>
+    </div>
     `
+    
     card.classList.add('card')
     card.setAttribute('id', item.id)
     card.innerHTML = content;
-
+    
     books.appendChild(card)
 
   })
-}
+};
 
 deckBuilder()
