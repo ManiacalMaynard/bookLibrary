05c9e706f3dd3caa8e5c1e6dd5eb66ddebb4c33c
ManiacalMaynard <maynard@maniacalindustries.com>
2022-04-25 17:49:51 -0700

    set up for addBook(), end of day

---
+++ addBook.js
@@ -0,0 +1,8 @@
+
+
+let modal = document.createElement('div')
+modal.className = 'modalFull'
+
+const test = () => {
+  console.log('ima test!')
+}
--- app.js
+++ app.js
@@ -9,12 +9,6 @@ function Book (image, title, author, link, inLibrary) {
   this.inLibrary = inLibrary
 };
 
-// Need to add a button that will add a new book object to the end of the bookList array, needs to call BUILDER again to re-populate the cardDeck div container
-// function addBook(input) {
-//   // let book = new Book(image, title, author, link, inLibrary);
-//   // bookList.push(book)
-//   console.log('adds a book!')
-// };
 
 // Need to add a removeBook() button that removes that book instance from the current working array and calls BUILDER to redraw the display
 // const removeBook = (id) => {
@@ -23,17 +17,8 @@ function Book (image, title, author, link, inLibrary) {
 // };
 
 // Need to add a libraryToggle() button that toggles the currentBook.inLibrary state
-// const libraryToggle = (item) => {
-//    item.inLibrary === true ?
-//    item.inLibrary = '□' :
-//    item.inLibrary = '🗹' ;
-// };
+const libraryToggle = (item) => {
 
-// Used for the card creation, inlibrary state display
-const libraryDisplay = (item) => {
-  return item.inLibrary === true ?
-  '🗹' :
-  '□' ;
 };
 
 // The mighty BUILDER function: Iterates through bookList array to build each card and add it to the cardDeck div container
@@ -55,9 +40,8 @@ function deckBuilder() {
 
     const buttons = document.createElement('div')
     const toggle = document.createElement('button')
-    toggle.innerText = libraryDisplay(item)
+    toggle.innerText = item.inLibrary === true ? '🗹' : '□' ;
     toggle.className = 'toggle'
-    toggle.addEventListener("click", libraryToggle(item.id));
     buttons.appendChild(toggle)
 
     // card.innerHTML = cardContent(item);
@@ -68,6 +52,7 @@ function deckBuilder() {
     card.appendChild(author)
     card.appendChild(buttons)
     books.appendChild(card)
+
 
   })
 };
